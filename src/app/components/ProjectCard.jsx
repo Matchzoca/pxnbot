f5286a0885992655f59efa41f757c8267f584bac
Match <104181165+Matchzoca@users.noreply.github.com>
2025-02-27 21:50:48 -0300

    Update ProjectCard.jsx

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -37,19 +37,19 @@ const ProjectCard = ({ title, description, exemplo_cmd, previewPXN }) => {
           <div className="bg-[#0f0f0f] bg-opacity-100 rounded-lg shadow-2xl max-w-md w-full space-y-4 p-6" style={{ boxShadow: ' rgba(42, 31, 86, 0.4) 0px 5px, rgba(42, 31, 86, 0.3) 0px 10px, rgba(42, 31, 86, 0.2) 0px 15px, rgba(42, 31, 86, 0.1) 0px 20px, rgba(42, 31, 86, 0.05) 0px 25px, rgba(0, 0, 0, 0.56) 0px 22px 70px 4px' }}> 
             <h2 className="text-2xl">Preview do Comando</h2>
             {previewPXN ? (
-              <div className="relative w-full h-[300px]">
-                <Image 
-                  src={previewPXN}
-                  alt={`Preview do comando ${title}`}
-                  fill
-                  style={{ objectFit: 'contain', borderRadius: '10px' }}
-                />
-              </div>
-            ) : (
-              <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
-                <span style={{ fontSize: '20px', color: '#ccc' }}>Sem imagem</span>
-              </div>
-            )}
+  <div className="relative w-full h-[300px]">
+    <Image 
+      src={previewPXN}
+      alt={`Preview do comando ${title}`}
+      fill
+      style={{ objectFit: 'contain', borderRadius: '10px' }}
+    />
+  </div>
+) : (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
+    <span style={{ fontSize: '20px', color: '#ccc' }}>Sem imagem</span>
+  </div>
+)}
             <button
               onClick={() => setShowModal(false)}
               className="px-4 py-2 bg-gradient-to-r from-purple-500 to-pink-500 text-white rounded-md shadow-md button"
