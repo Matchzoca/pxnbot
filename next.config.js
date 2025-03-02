8593ca8f47dd2d958b66259a1e7d752420a9948a
Match <104181165+Matchzoca@users.noreply.github.com>
2025-03-02 17:58:05 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'pxnbot-j68r5rqn8-slowpixels.vercel.app'],
+    domains: ['localhost', 'pxnbot-j68r5rqn8-slowpixels.vercel.app', 'pxnbot.vercel.app'],
     unoptimized: true,
     remotePatterns: [
       {
