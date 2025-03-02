5fb36020b199c9eddd2ba3459ce254facc68a44d
Match <104181165+Matchzoca@users.noreply.github.com>
2025-03-02 18:28:23 -0300

    Update ProjectCard.jsx

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
 
 const ProjectCard = ({ title, description, exemplo_cmd, previewPXN }) => {
   const [linkText, setLinkText] = useState("Como usar");
@@ -34,22 +33,13 @@ const ProjectCard = ({ title, description, exemplo_cmd, previewPXN }) => {
       {showModal && (
         <div className="fixed z-10 inset-0 overflow-y-auto">
           <div className="flex items-center justify-center min-h-screen">
-            <div className="bg-[#0f0f0f] bg-opacity-100 rounded-lg shadow-2xl max-w-md w-full space-y-4 p-6">
+            <div className="bg-[#0f0f0f] bg-opacity-100 rounded-lg shadow-2xl max-w-md w-full space-y-4 p-6" style={{ boxShadow: ' rgba(42, 31, 86, 0.4) 0px 5px, rgba(42, 31, 86, 0.3) 0px 10px, rgba(42, 31, 86, 0.2) 0px 15px, rgba(42, 31, 86, 0.1) 0px 20px, rgba(42, 31, 86, 0.05) 0px 25px, rgba(0, 0, 0, 0.56) 0px 22px 70px 4px' }}> 
               <h2 className="text-2xl">Preview do Comando</h2>
               {previewPXN ? (
-                <div className="relative w-full h-[300px]">
-                  <Image 
-                    src={previewPXN}
-                    alt={`Preview do comando ${title}`}
-                    layout="responsive"
-                    width={500}
-                    height={300}
-                    style={{ objectFit: 'contain', borderRadius: '10px' }}
-                  />
-                </div>
+                <img src={previewPXN} alt={`Preview do comando ${title}`} style={{ width: '100%', height: 'auto', borderRadius: '10px' }} />
               ) : (
-                <div className="flex justify-center items-center h-[200px]">
-                  <span className="text-xl text-gray-400">Sem imagem</span>
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
+                  <span style={{ fontSize: '20px', color: '#ccc' }}>Sem imagem</span>
                 </div>
               )}
               <button
