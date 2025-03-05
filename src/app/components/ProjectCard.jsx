335187124205812a5f5e1124ff60798c4ca84116
Match <104181165+Matchzoca@users.noreply.github.com>
2025-03-05 02:23:13 -0300

    Update ProjectCard.jsx

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 const ProjectCard = ({ title, description, exemplo_cmd, previewPXN }) => {
   const [linkText, setLinkText] = useState("Como usar");
@@ -31,12 +32,20 @@ const ProjectCard = ({ title, description, exemplo_cmd, previewPXN }) => {
         </div>
       </div>
       {showModal && (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="fixed z-50 inset-0 overflow-y-auto">
           <div className="flex items-center justify-center min-h-screen">
             <div className="bg-[#0f0f0f] bg-opacity-100 rounded-lg shadow-2xl max-w-md w-full space-y-4 p-6" style={{ boxShadow: ' rgba(42, 31, 86, 0.4) 0px 5px, rgba(42, 31, 86, 0.3) 0px 10px, rgba(42, 31, 86, 0.2) 0px 15px, rgba(42, 31, 86, 0.1) 0px 20px, rgba(42, 31, 86, 0.05) 0px 25px, rgba(0, 0, 0, 0.56) 0px 22px 70px 4px' }}> 
               <h2 className="text-2xl">Preview do Comando</h2>
               {previewPXN ? (
-                <img src={previewPXN} alt={`Preview do comando ${title}`} style={{ width: '100%', height: 'auto', borderRadius: '10px' }} />
+                <div className="relative w-full h-[300px]">
+                  <Image 
+                    src={previewPXN}
+                    alt={`Preview do comando ${title}`}
+                    layout="fill"
+                    objectFit="contain"
+                    style={{ borderRadius: '10px' }}
+                  />
+                </div>
               ) : (
                 <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '200px' }}>
                   <span style={{ fontSize: '20px', color: '#ccc' }}>Sem imagem</span>
